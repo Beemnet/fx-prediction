8f05046f04472870975b09d59e928bb26d1f5423
Beemnet <bemnetteklu@gmail.com>
2025-05-07 23:05:10 +0200

    minute by minute updates, automation not done

--- scripts/scraping/yahoo_finance.py
+++ scripts/scraping/yahoo_finance.py
@@ -1,6 +1,6 @@
 import yfinance as yf
 
 # Yahoo ticker for USD to EUR is "EURUSD=X"
-data = yf.download("EURUSD=X", period="5d", interval="1h")
+data = yf.download("EURUSD=X", period="5d", interval="1m", auto_adjust=True) 
+# Valid intervals: [1m, 2m, 5m, 15m, 30m, 60m, 90m, 1h, 4h, 1d, 5d, 1wk, 1mo, 3mo]
 print(data.tail())
-
